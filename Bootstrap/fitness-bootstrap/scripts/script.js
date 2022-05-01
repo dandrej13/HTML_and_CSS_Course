87990a2e31d063bc631a27485988b885c0fa1ef4
dandrej13 <dimitrievsk.andrej@hotmail.com>
2022-05-01 20:26:29 +0200

    21-04

---
+++ Bootstrap/fitness-bootstrap/scripts/script.js
@@ -0,0 +1,11 @@
+/*
+    Student Name: Your name
+    File Name: script.js 
+    Date: Today’s date
+*/
+
+// jQuery for hero image to consume the header window space
+
+$(document).ready(function() {
+            $(".hero").height($(window).height())
+    });
