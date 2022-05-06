93c896eeec4f1477a98355cfa6fdc40c87d8787f
dandrej13 <dimitrievsk.andrej@hotmail.com>
2022-05-06 18:08:17 +0200

    05.05.22

---
+++ 12_JavaScript_Essentials/ex01/main.js
@@ -0,0 +1,41 @@
+let language = "Javascript";
+        let activity = `Let's learn ${language}`;
+        console.log(activity); 
+
+
+let baby = "Lina";
+
+let nephew = "Baby's name is" + " " + baby ;
+console.log(nephew)
+
+
+/*  */
+
+
+let testVariable = 1;
+let variableTypeTest1 = typeof testVariable;
+let variableTypeTest2 = typeof (testVariable);
+
+console.log( variableTypeTest1)
+console.log ( variableTypeTest2)
+
+let nrToStr = 6;
+nrToStr = String(nrToStr);
+console.log(nrToStr, typeof (nrToStr));
+
+let strToNr = "5";
+strToNr = Number(strToNr);
+console.log(strToNr, typeof (strToNr));
+
+let strToBool = "any string will return true";
+strToBool = Boolean(strToBool);
+console.log(strToBool, typeof (strToBool));
+
+let nullToNr = null;
+nullToNr = Number(nullToNr);
+console.log(nullToNr, typeof (nullToNr));
+
+
+
+
+
---
+++ 12_JavaScript_Essentials/ex02.02/main.js
@@ -0,0 +1,10 @@
+let fname = "Andrej";
+let age = 23;
+let ext= true;
+
+console.log('My name is ' + fname + ', I am ' + age + ' years old and I can code JavaScript: ' + ext) 
+
+let a = prompt();
+console.log(a);
+
+let first
